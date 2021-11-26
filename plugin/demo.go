e1ef0d2d198b6bd5cd49c1a47f864ed99912a1c1
方友松 <9569356+truexf@user.noreply.gitee.com>
2021-11-26 12:21:11 +0800

    + plugin/demo

---
+++ plugin/demo.go
@@ -0,0 +1,42 @@
+package plugin
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/truexf/goutil"
+	"github.com/truxf/paddy"
+)
+
+type DemoPlugin struct {
+}
+
+// 唯一身份ID
+func (m *DemoPlugin) ID() string {
+	return "demo"
+}
+
+// 在http请求接收完成后介入
+// hijacked 是否劫持：true则必须实现respWriter写响应；false时不准向respWriter写响应，可以返回backend(此时框架直接去请求backend而不再走location匹配流程，否则框架执行location匹配)
+func (m *DemoPlugin) RequestHeaderCompleted(req *http.Request, respWriter http.ResponseWriter, context goutil.Context) (hijacked bool, backend string, err goutil.Error) {
+	if req.URL.Path == "/ping" {
+		respWriter.WriteHeader(200)
+		respWriter.Write([]byte("pong"))
+		return true, "", paddy.ErrorNoError
+	} else {
+		return false, "", paddy.ErrorNoError
+	}
+}
+
+// 框架在得到响应后，给客户端发送响应之前介入
+// hijacked 是否劫持：true则必须实现respWriter写响应；false时，不准向respWriter写响应，可以返回newResponse(此时框架以newResponse写响应，否则以originResponse写响应）
+func (m *DemoPlugin) ResponseHeaderCompleted(originResponse *http.Response, respWriter http.ResponseWriter, context goutil.Context) (hijacked bool, newResponse *http.Response, err goutil.Error) {
+	var bodyBuf bytes.Buffer
+	if _, err := io.Copy(&bodyBuf, originResponse.Body); err != nil {
+		return false, nil, goutil.NewErrorf(paddy.ErrCodeDemo, paddy.ErrMsgDemo, err.Error())
+	}
+	bodyBuf.WriteString("append demo data to response body")
+	originResponse.Body = io.NopCloser(&bodyBuf)
+	return true, originResponse, paddy.ErrorNoError
+}
